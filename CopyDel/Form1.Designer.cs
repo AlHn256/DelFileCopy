a32f80a4943734801c9cb019dda65f96e4c7e331
AlHn256 <AlexHn256@yandex.ru>
2023-03-14 12:50:22 +0300

    LoadingProgect

---
+++ CopyDel/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CopyDel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textdir = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(725, 15);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(69, 23);
+            this.test.TabIndex = 27;
+            this.test.Text = "Del Copy";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(782, 309);
+            this.richTextBox1.TabIndex = 28;
+            this.richTextBox1.Text = "";
+            // 
+            // textdir
+            // 
+            this.textdir.Location = new System.Drawing.Point(87, 17);
+            this.textdir.Name = "textdir";
+            this.textdir.Size = new System.Drawing.Size(632, 20);
+            this.textdir.TabIndex = 29;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Show Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 362);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textdir);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textdir;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CopyDel/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CopyDel
+{
+    public partial class Form1 : Form
+    {
+        string Dir = @"F:\img\test\";
+        public Form1()
+        {
+            InitializeComponent();
+
+            textdir.Text = Dir;
+            this.AllowDrop = true;
+            richTextBox1.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(WindowsForm_DragEnter);
+            this.DragDrop += new DragEventHandler(WindowsForm_DragDrop);
+            richTextBox1.DragEnter += new DragEventHandler(WindowsForm_DragEnter);
+            richTextBox1.DragDrop += new DragEventHandler(WindowsForm_DragDrop);
+        }
+
+        List<CopyList> CList;
+
+        public class CopyList
+        {
+            public string File { get; set; }
+            public string Hesh { get; set; }
+            public int Copy { get; set; }
+
+            public CopyList(string File, string Hesh, int Copy)
+            {
+                this.File = File;
+                this.Hesh = Hesh;
+                this.Copy = Copy;
+            }
+        }
+
+        private string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                //string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                string result = BitConverter.ToString(checkSum);
+                return result;
+            }
+        }
+
+
+        private void FindCopy(bool Del)
+        {
+            richTextBox1.Text = "";
+            CList = new List<CopyList>();
+            string text="";
+
+            if (System.IO.Directory.Exists(Dir) == true)
+            {
+                text += "\nDir - " + Dir;
+                string[] dirs = Directory.GetFiles(Dir, "*.*");
+
+                text += "\n" + dirs.Length + " files";
+
+                if (dirs.Length != 0)
+                {
+                    foreach (string file in dirs)
+                    {
+                        string md5 = ComputeMD5Checksum(file);
+                        CopyList elm = new CopyList(file, md5,-1); CList.Add(elm);
+                    }
+
+                    int i = 0, j = 0;
+                    for (i = 0; i < CList.Count() - 1; i++)
+                    {
+                        string HeshI = CList[i].Hesh;
+                        for (j = i + 1; j < CList.Count(); j++)
+                        {
+                            if (CList[j].Copy == -1 && HeshI == CList[j].Hesh)
+                            {
+                                CList[i].Copy = i;
+                                CList[j].Copy = i;
+                            }
+                        }
+                    }
+
+                    i = 0; j = 0;
+                    for (i = 0; i < CList.Count; i++)
+                    {
+                        if (CList[i].Copy != -1 && CList[i].Copy != i)
+                        {
+                            j++;
+                            text += "\n" + i + " " + CList[i].Copy + " " + CList[i].File + " " + CList[i].Hesh + "    -    COPY";
+                            if (Del == true) { File.Delete(CList[i].File); text += "\n" + CList[i].File + "    -   DEL"; }
+                        }
+                    }
+                    text += "\n" + j + " Копий";
+                }
+            }
+            richTextBox1.Text += text;
+        }
+
+        private void test_Click(object sender, EventArgs e){FindCopy(true);}
+        private void button1_Click(object sender, EventArgs e){FindCopy(false);}
+        void WindowsForm_DragEnter(object sender, DragEventArgs e){if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;}
+        void WindowsForm_DragDrop(object sender, DragEventArgs e)
+        {
+            richTextBox1.Text = "";
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                FileAttributes attr = File.GetAttributes(file);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory) textdir.Text =  file;
+                else textdir.Text=Path.GetDirectoryName(file);
+                Dir = textdir.Text;
+            }
+        }
+    }
+}
